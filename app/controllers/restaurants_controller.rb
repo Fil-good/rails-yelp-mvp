4bc9b4ba8fb79938ca28c0ab4fa89385be16dcef
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-11 14:20:45 +0100

    backup to github

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.find(params[:restaurant_id])
+  end
+
+  def show
+  end
+
+  def new
+  end
+  def create
+  end
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :rating, presence: true, acceptance: { accept: (0..5) }
+  validates :rating, presence: true, acceptance: { accept: (0..5) }, numericality: { only_integer: true }
   validates :content, presence: true
+
 
 end
